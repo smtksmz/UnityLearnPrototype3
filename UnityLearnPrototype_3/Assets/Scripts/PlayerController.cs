367ceb3253899d4316a2cb594badac87ea5a93a9
Samet Kosmaz <ksmzsmt@gmail.com>
2024-04-15 21:17:05 +0300

    add RepeatBackground.cs and fix other scripts

--- UnityLearnPrototype_3/Assets/Scripts/PlayerController.cs
+++ UnityLearnPrototype_3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround=true;
+    public bool gameOver;
 
     void Start()
     {
@@ -26,6 +27,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
